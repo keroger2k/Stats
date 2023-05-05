f764edfc21bdf3463ffcfe795ea7385c3cd73d8e
kyle.rogers <kyle.rogers@gmail.com>
2023-05-04 20:47:12 -0500

    losing track

--- stats.web/src/App.tsx
+++ stats.web/src/App.tsx
@@ -1,9 +1,14 @@
 import TopNavbar from './components/TopNavbar';
+import MainContainer from './components/MainContainer';
+import { ThemeProvider } from 'react-bootstrap';
 import './App.scss';
 
 function App() {
     return (
+    <>
         <TopNavbar></TopNavbar>
+        <MainContainer></MainContainer>
+    </>
   );
 }
 

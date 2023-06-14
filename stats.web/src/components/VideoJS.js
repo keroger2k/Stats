c427c8be4a474d9822bd131336656cbd578573d9
kroger03 <kroger03@peraton.com>
2023-06-14 11:45:10 -0500

    little cleaner.

--- stats.web/src/components/VideoJS.js
+++ stats.web/src/components/VideoJS.js
@@ -61,8 +61,20 @@ export const VideoJS = (props: any) => {
     return (
         <div data-vjs-player>
             <div ref={videoRef} />
-            <button onClick={ goBackwardOneFrame }>{`<<`}</button>
-            <button onClick={ goForwardOneFrame }>{`>>`}</button>
+            <div className="d-flex justify-content-center">
+                <button className="btn btn-secondary" onClick={goBackwardOneFrame}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-rewind" viewBox="0 0 16 16">
+                        <path d="M9.196 8 15 4.633v6.734L9.196 8Zm-.792-.696a.802.802 0 0 0 0 1.392l6.363 3.692c.52.302 1.233-.043 1.233-.696V4.308c0-.653-.713-.998-1.233-.696L8.404 7.304Z" />
+                        <path d="M1.196 8 7 4.633v6.734L1.196 8Zm-.792-.696a.802.802 0 0 0 0 1.392l6.363 3.692c.52.302 1.233-.043 1.233-.696V4.308c0-.653-.713-.998-1.233-.696L.404 7.304Z" />
+                    </svg>
+                </button>&nbsp;
+                <button className="btn btn-secondary" onClick={goForwardOneFrame}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-fast-forward" viewBox="0 0 16 16">
+                        <path d="M6.804 8 1 4.633v6.734L6.804 8Zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C.713 12.69 0 12.345 0 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692Z" />
+                        <path d="M14.804 8 9 4.633v6.734L14.804 8Zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C8.713 12.69 8 12.345 8 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692Z" />
+                    </svg>
+                </button>
+            </div>
         </div>
     );
 }

818cfad106d536ba480632e71f779288b1ad73ef
kroger03 <kroger03@peraton.com>
2023-06-15 14:27:19 -0500

    bug

--- Stats.Models/VideoAsset.cs
+++ Stats.Models/VideoAsset.cs
@@ -14,7 +14,7 @@
         public string schedule_event_id { get; set; } = null!;
         public DateTime created_at { get; set; }
         public string audience_type { get; set; } = null!;
-        public int duration { get; set; }
+        public int? duration { get; set; }
         public DateTime ended_at { get; set; }
         public string thumbnail_url { get; set; } = null!;
     }
